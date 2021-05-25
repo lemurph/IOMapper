13668bfe7ff77f34b10947337f5edaa60d147ee4
loganmurphy11 <loganmurphy151@gmail.com>
2021-05-25 10:05:40 -0300

    Cleaned up duplicate files

--- register_types.h
+++
@@ -1,5 +0,0 @@
-/* register_types.h */
-
-void register_godot_mapper_types();
-void unregister_godot_mapper_types();
-/* the word in the middle must be the same as the module folder name; this may cause issues with hyphens and underscores */
