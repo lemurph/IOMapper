97465c17f3067091edf645d56f66fc1d719eae57
loganmurphy11 <loganmurphy151@gmail.com>
2021-05-25 10:13:32 -0300

    Cleaned up test projects

--- godot_examples/poll_test/Node2D.gd
+++ godot_examples/poll_test/Node2D.gd
@@ -2,17 +2,8 @@ extends Node2D
 
 
 var dev1 = GodotMapper.new();
-var dev2 = GodotMapper.new();
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(dev1.init("test1"));
-	dev2.init("test2");
-	
-	dev1.poll();
-	#dev2.add_sig("IN", "test_in", 1, "FLOAT");
-	#dev1.poll();
-	#dev2.poll();
-	
-	
-
+	dev1.init("test1")
+	dev1.poll()
