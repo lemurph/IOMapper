c7bb4ea58aa02895226f7824e96205b2979dd51f
loganmurphy11 <loganmurphy151@gmail.com>
2021-05-27 09:42:21 -0300

    Added device deconstructor, more tests

--- godot_examples/control_test/test1.gd
+++ godot_examples/control_test/test1.gd
@@ -2,22 +2,26 @@ extends Node2D
 
 var dev = GodotMapper.new()
 var done = false
-var value = 0
+var value = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	dev.init("test")
 	dev.add_sig("OUT", "test_out", 1, "FLOAT")
-	dev.add_sig("IN", "test_in", 1, "FLOAT")
 	
-	while (!done):
-		dev.poll_blocking(10)
-		dev.set_value("OUT", 5)
+
 
 func _process(_delta):
-	dev.poll()
-	value += 0.5
+	if (Input.is_action_pressed("ui_right")):
+		value += 1
+	elif (Input.is_action_pressed("ui_left")):
+		value -= 1
+
+	$Label.text = value
 	dev.set_value("OUT", value)
+
+
+
 
 # Called every frame. 'delta' is the elapsed tisme since the previous frame.
 #func _process(delta):
--- godot_examples/poll_test/Node2D.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-var dev1 = GodotMapper.new();
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	dev1.init("test1")
-	dev1.poll()
