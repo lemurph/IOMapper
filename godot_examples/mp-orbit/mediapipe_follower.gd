8f01e1bde982d1b061b045efae727b54a85d8931
Matt Peachey <peacheym@dal.ca>
2021-06-08 11:04:16 -0300

    Rename .gd script and update contents for cleaner code.

--- godot_examples/mp-orbit/mediapipe_follower.gd
+++ godot_examples/mp-orbit/mediapipe_follower.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+# Get window dimensions
+var width = ProjectSettings.get_setting("display/window/size/width")
+var height = ProjectSettings.get_setting("display/window/size/height")
+
 var dev = gdMprDevice.new()
+
 # I'm pretty sure these are unused, but too scared to remove
 export var x_pos = 0
 export var y_pos = 0
@@ -10,7 +15,9 @@ func _ready():
 	dev.init("orbit")
 	dev.add_sig(gdMprDevice.INCOMING, "input_X", 1, gdMprDevice.FLOAT)
 	dev.add_sig(gdMprDevice.INCOMING, "input_Y", 1, gdMprDevice.FLOAT)
-	dev.set_value_float("input_X", 40.0)
+
+	# Script seems to break without these -- TODO: Ask Logan why? 
+	dev.set_value_float("input_X", 0.0)
 	dev.set_value_float("input_Y", 0.0)
 
 
@@ -20,12 +27,10 @@ func _physics_process(delta):
 	# Get values from both X and Y signals
 	var x_val = dev.value_float("input_X")
 	var y_val = dev.value_float("input_Y")
-	
-	var width = ProjectSettings.get_setting("display/window/size/width")
-	var height = ProjectSettings.get_setting("display/window/size/height")
-	
-	print(height)
+
+	var new_pos = Vector2(x_val*width, y_val * height)
+	print(new_pos)
 	
 	# Move towards where the tracked values are (scaled to window dimensions)
 	var speed = 300 # Change this to increase it to more units/second
-	$Sprite.position = $Sprite.position.move_toward(Vector2(x_val * width, y_val * height), delta * speed)	
+	$Sprite.position = $Sprite.position.move_toward(new_pos, delta * speed)	
