e0c2570552024c6c4a20ed6bbdb33708db372a71
Logan Murphy <loganmurphy151@gmail.com>
2021-06-18 10:22:30 -0300

    Fixed naming issue in register_types.h

--- IOMapper/register_types.h
+++ IOMapper/register_types.h
@@ -1,5 +1,5 @@
 /* register_types.h */
 
-void register_godot_mapper_types();
-void unregister_godot_mapper_types();
+void register_IOMapper_types();
+void unregister_IOMapper_types();
 /* the word in the middle must be the same as the module folder name; this may cause issues with hyphens*/
