5320cf4d08c3206a2f0c36a48424efe2e686754e
Logan Murphy <loganmurphy151@gmail.com>
2021-08-18 13:05:18 -0300

    Added enums for Direction/Type, isolated IOMapper class, and created example script

--- Unity/importMapper.cs
+++ Unity/importMapper.cs
@@ -5,61 +5,37 @@ using UnityEngine;
 using System.Runtime.InteropServices;
 using System.Text;
 
-public class Mapper : MonoBehaviour
-{
+namespace Mapper {
 
-    /*
-        DIRECTION
-        IN = 1
-        OUT = 2
-
-        MPR_BOOL            = 'b',   0x62 !< Boolean value. 
-        MPR_TYPE            = 'c',   0x63 !< libmapper data type. 
-        MPR_DBL             = 'd',   0x64 !< 64-bit floating point. 
-        MPR_FLT             = 'f',   0x66 !< 32-bit floating point. 
-        MPR_INT64           = 'h',   0x68 !< 64-bit integer. 
-        MPR_INT32           = 'i',   0x69 !< 32-bit integer
-        MPR_STR             = 's',   0x73 !< String. 
-        MPR_TIME            = 't',   0x74 !< 64-bit NTP timestamp. 
-        MPR_PTR             = 'v',   0x76 !< pointer. 
-        MPR_NULL            = 'N'    0x4E !< NULL value. 
-
-    */
-
-    IntPtr dev = IntPtr.Zero;
-    IntPtr sig = IntPtr.Zero;
-    private float myValue;
-
-    // Define libmapper function for new device
-    [DllImport ("mapper")]
-    private static extern IntPtr mpr_dev_new(String name_prefix, int graph);
-    [DllImport ("mapper")]
-    private static extern int mpr_dev_poll(IntPtr dev, int block_ms);
-    [DllImport ("mapper")]
-    private static extern IntPtr mpr_sig_new(IntPtr parent_dev, int dir, String name, int length,
-                    int type, int unit, int min, int max,
-                    int num_inst, int h, int events);
-    [DllImport ("mapper")]                
-    private static extern float mpr_sig_get_value(IntPtr signal, int instance, int time);
-
-    // Start is called before the first frame update
-    void Start()
+    public class IOMapper
     {
-        dev = mpr_dev_new("Test", 0);
-        sig = mpr_sig_new(dev, 1, "test_sig", 1, 102, 0, 0, 0, 0, 0, 0);
-    }
+        public enum Direction {
+            INCOMING = 1,
+            OUTGOING = 2,
+            ANY = 3,
+            BOTH = 4,
+        }
+        public enum Type {
+            DOUBLE = 100,
+            FLOAT = 102,
+            INT32 = 105,
+        }
 
-    // Update is called once per frame
-    void Update()
-    {   
-        mpr_dev_poll(dev, 0);   
-        myValue = mpr_sig_get_value(sig, 0, 0);     
-    }
+        // Define libmapper function for new device
+        [DllImport ("mapper")]
+        public static extern IntPtr mpr_dev_new(String name_prefix, int graph);
+        [DllImport ("mapper")]
+        public static extern int mpr_dev_poll(IntPtr dev, int block_ms);
+        [DllImport ("mapper")]
+        public static extern IntPtr mpr_sig_new(IntPtr parent_dev, Direction dir, String name, int length,
+                        Type type, int unit, int min, int max,
+                        int num_inst, int h, int events);
+        [DllImport ("mapper")]                
+        public static extern float mpr_sig_get_value(IntPtr signal, int instance, int time);
 
-    // Normalized update function, called once every physics update (instead of per frame)
-    private void FixedUpdate()
-    {
-       
     }
-
 }
+
+
+
+
