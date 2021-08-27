3732fd322547ca21245178a08cf11e1125325d5f
Logan Murphy <loganmurphy151@gmail.com>
2021-08-27 10:50:05 -0300

    Added more libmapper functions, added function overloads

--- Unity/MapperExample.cs
+++ Unity/MapperExample.cs
@@ -19,26 +19,26 @@ public class MapperExample : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
+        // Store particle system in ps
         ps = GetComponent<ParticleSystem>();
-        dev = IOMapper.mpr_dev_new("Test", 0);
+        
+        dev = IOMapper.mpr_dev_new("Test");
         sig = IOMapper.mpr_sig_new(dev, IOMapper.Direction.INCOMING, "test_sig", 1, IOMapper.Type.FLOAT);
     }
     
     // Update is called once per frame
     void Update()
     {   
-        IOMapper.mpr_dev_poll(dev, 0);
-        IntPtr myValue = IOMapper.mpr_sig_get_value(sig, 0, 0);
+        IOMapper.mpr_dev_poll(dev);
+        IntPtr myValue = IOMapper.mpr_sig_get_value(sig);
         if (myValue != IntPtr.Zero) {
             unsafe {
                 float fval = *(float*)myValue;
+                // Change value of particle system gravity
                 var main = ps.main;
                 main.gravityModifier = fval/50;  
             }
-        }    
-
-        
-         
+        }     
     }
     
     // Normalized update function, called once every physics update (instead of per frame)
